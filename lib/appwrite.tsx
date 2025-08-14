b6d59ffd8dfe831e44b170c20f522918f8b77434
mzhossai <mohossain2002@gmail.com>
2025-08-14 15:34:02 -0400

    fixed some issue with env. Just learned you need to use process.env.'env-variable' to use dotenv stuff, for EXPO. The more you know :)

--- lib/appwrite.tsx
+++ lib/appwrite.tsx
@@ -1,12 +1,10 @@
 import { Account, Client } from 'react-native-appwrite';
-import 'dotenv/config';
 
 
 const client = new Client()
     .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
     .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
     .setDevKey(process.env.EXPO_PUBLIC_APPWRITE_DEVKEY!);
-
 
 
 export const account = new Account(client);
